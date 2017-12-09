1f801e38c79042384e85ff55a00c4a9393f22904
Jacek Bilski <jacek.bilski@gmail.com>
2017-12-09 19:25:26 +0100

    Upgrading Clojure itself

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject app.clj "0.1.0-SNAPSHOT"
-  :dependencies [[org.clojure/clojure "1.8.0"]
+  :dependencies [[org.clojure/clojure "1.9.0"]
                  [com.layerware/hugsql "0.4.8"]
                  [org.postgresql/postgresql "42.1.4"]]
   :main ^:skip-aot app
